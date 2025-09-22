4f9041fe0f8e20c3c3271c4ef79affdccd242037
mallory-scotton <mallory.scotton@epitech.eu>
2025-09-22 13:33:25 +0200

    feat: implement signal handling for graceful shutdown on SIGINT and SIGTERM

--- Engine/Source/Main.cpp
+++ Engine/Source/Main.cpp
@@ -1,11 +1,35 @@
 ///////////////////////////////////////////////////////////////////////////////
 // Dependencies
 ///////////////////////////////////////////////////////////////////////////////
+#include <csignal>
 #include <Engine.hpp>
+
+///////////////////////////////////////////////////////////////////////////////
+// Graceful exit to ensure shutdown of the engine when SIGINT or SIGTERM
+///////////////////////////////////////////////////////////////////////////////
+void SignalHandler(int signal)
+{
+    if (signal == SIGINT || signal == SIGTERM)
+    {
+        if (Engine::IsInitialized())
+        {
+            Engine::SetExitCode(TKD_EXIT_SUCCESS);
+            Engine::SetExitMessage("Graceful shutdown");
+            Engine::Shutdown();
+        }
+        std::cerr
+            << "\nEngine terminated unexpectedly, graceful shutdown initiated"
+            << std::endl;
+        std::exit(Engine::GetExitCode());
+    }
+}
 
 ///////////////////////////////////////////////////////////////////////////////
 int main(int argc, char* argv[])
 {
+    std::signal(SIGINT, SignalHandler);
+    std::signal(SIGTERM, SignalHandler);
+
     try
     {
         if (Engine::Initialize(argc, argv))
