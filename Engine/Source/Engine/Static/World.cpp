fbd2ac47be5f61008b9c2b63395d9618e9417edf
mallory-scotton <mallory.scotton@epitech.eu>
2025-09-26 16:06:17 +0200

    feat: initialize s_currentWorld with a new UWorld instance

--- Engine/Source/Engine/Static/World.cpp
+++ Engine/Source/Engine/Static/World.cpp
@@ -11,7 +11,7 @@ namespace tkd::__internal
 
 ///////////////////////////////////////////////////////////////////////////////
 std::vector<std::unique_ptr<UWorld>> World::s_worlds = {};
-std::unique_ptr<UWorld> World::s_currentWorld = nullptr;
+std::unique_ptr<UWorld> World::s_currentWorld = std::make_unique<UWorld>();
 
 ///////////////////////////////////////////////////////////////////////////////
 UWorld* World::Get(void) { return s_currentWorld.get(); }
