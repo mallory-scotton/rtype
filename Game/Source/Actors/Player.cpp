ea327f25242e7caac3a29c6326ae923b45c68bd2
mallory-scotton <mallory.scotton@epitech.eu>
2025-10-04 16:38:52 +0200

    feat: implement player movement and firing input handling in BP_Player::Tick

--- Game/Source/Actors/Player.cpp
+++ Game/Source/Actors/Player.cpp
@@ -20,7 +20,43 @@ BP_Player::BP_Player(void)
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-void BP_Player::Tick(Float32 deltaTime) { TKD_UNUSED(deltaTime); }
+void BP_Player::Tick(Float32 deltaTime)
+{
+    auto hAxis = Engine::Inputs.GetAxis("HorizontalMoves");
+    auto vAxis = Engine::Inputs.GetAxis("VerticalMoves");
+    auto fire = Engine::Inputs.GetAction("Fire");
+
+    if (hAxis)
+    {
+        float scale = hAxis->GetScale();
+        if (scale != 0.0f)
+        {
+            FTransform transform = GetTransform();
+            FVector3 position = transform.GetPosition();
+            position.x += scale * speed * deltaTime;
+            transform.SetPosition(position);
+            SetTransform(transform);
+        }
+    }
+
+    if (vAxis)
+    {
+        float scale = vAxis->GetScale();
+        if (scale != 0.0f)
+        {
+            FTransform transform = GetTransform();
+            FVector3 position = transform.GetPosition();
+            position.y += scale * speed * deltaTime;
+            transform.SetPosition(position);
+            SetTransform(transform);
+        }
+    }
+
+    if (fire)
+    {
+        // Process fire action input
+    }
+}
 
 ///////////////////////////////////////////////////////////////////////////////
 IMPLEMENT_CLASS_WITH_SUPER(BP_Player, APawn)
