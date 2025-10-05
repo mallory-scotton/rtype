87ef08756fbb3f8fc5a9c4e8099663b9b7eb75b6
mallory-scotton <mallory.scotton@epitech.eu>
2025-10-06 01:16:57 +0200

    refactor: comment out unused player input handling in Tick function

--- Game/Source/Actors/BP_Player.cpp
+++ Game/Source/Actors/BP_Player.cpp
@@ -20,40 +20,41 @@ BP_Player::BP_Player(void)
 ///////////////////////////////////////////////////////////////////////////////
 void BP_Player::Tick(Float32 deltaTime)
 {
-    auto hAxis = Engine::Inputs.GetAxis("HorizontalMoves");
-    auto vAxis = Engine::Inputs.GetAxis("VerticalMoves");
-    auto fire = Engine::Inputs.GetAction("Fire");
+    TKD_UNUSED(deltaTime);
+    // auto hAxis = Engine::Inputs.GetAxis("HorizontalMoves");
+    // auto vAxis = Engine::Inputs.GetAxis("VerticalMoves");
+    // auto fire = Engine::Inputs.GetAction("Fire");
 
-    if (hAxis)
-    {
-        float scale = hAxis->GetScale();
-        if (scale != 0.0f)
-        {
-            FTransform transform = GetTransform();
-            FVector3 position = transform.GetPosition();
-            position.x += scale * speed * deltaTime;
-            transform.SetPosition(position);
-            SetTransform(transform);
-        }
-    }
+    // if (hAxis)
+    // {
+    //     float scale = hAxis->GetScale();
+    //     if (scale != 0.0f)
+    //     {
+    //         FTransform transform = GetTransform();
+    //         FVector3 position = transform.GetPosition();
+    //         position.x += scale * speed * deltaTime;
+    //         transform.SetPosition(position);
+    //         SetTransform(transform);
+    //     }
+    // }
 
-    if (vAxis)
-    {
-        float scale = vAxis->GetScale();
-        if (scale != 0.0f)
-        {
-            FTransform transform = GetTransform();
-            FVector3 position = transform.GetPosition();
-            position.y += scale * speed * deltaTime;
-            transform.SetPosition(position);
-            SetTransform(transform);
-        }
-    }
+    // if (vAxis)
+    // {
+    //     float scale = vAxis->GetScale();
+    //     if (scale != 0.0f)
+    //     {
+    //         FTransform transform = GetTransform();
+    //         FVector3 position = transform.GetPosition();
+    //         position.y += scale * speed * deltaTime;
+    //         transform.SetPosition(position);
+    //         SetTransform(transform);
+    //     }
+    // }
 
-    if (fire)
-    {
-        // Process fire action input
-    }
+    // if (fire)
+    // {
+    //     // Process fire action input
+    // }
 }
 
 ///////////////////////////////////////////////////////////////////////////////
