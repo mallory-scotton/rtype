7fde6b17c30d2ec04787993445e041d530bf1f43
mallory-scotton <mallory.scotton@epitech.eu>
2025-10-07 21:45:20 +0200

    feat: implement BP_Projectile class with movement and animation functionality

---
+++ Game/Source/Core/Weapons/BP_Projectile.cpp
@@ -0,0 +1,47 @@
+///////////////////////////////////////////////////////////////////////////////
+// Dependencies
+///////////////////////////////////////////////////////////////////////////////
+#include <Core/Weapons/BP_Projectile.hpp>
+
+///////////////////////////////////////////////////////////////////////////////
+// Namespace tkd
+///////////////////////////////////////////////////////////////////////////////
+namespace tkd
+{
+
+///////////////////////////////////////////////////////////////////////////////
+BP_Projectile::BP_Projectile(void)
+    : AActor("BP_Projectile")
+{
+    auto SpriteComponent =
+        AddComponent<UAnimatedSpriteComponent>("AnimatedSpriteComponent");
+    SpriteComponent->SetTexturePath("Game/Assets/Images/T_Projectiles.png");
+
+    // Define PROJECTILE animation
+    FAnimation2D A_PROJECTILE("PROJECTILE", true);
+    A_PROJECTILE.AddFrame(FAnimation2DFrame(FRectanglei(233, 17, 34, 34)));
+    SpriteComponent->AddAnimation(A_PROJECTILE);
+
+    // Set the default animation to PROJECTILE
+    SpriteComponent->Play("PROJECTILE", true);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void BP_Projectile::Tick(Float32 deltaTime)
+{
+    // Call Super Tick
+    Super::Tick(deltaTime);
+
+    // Move the projectile forward
+    FTransform transform = GetTransform();
+    transform.Translate(FVector3(500.0f * deltaTime, 0.0f, 0.0f));
+    SetTransform(transform);
+
+    // If the projectile goes out of bounds, destroy it
+    if (transform.GetPosition().x > 2000.0f) { MarkForDeletion(); }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+IMPLEMENT_CLASS_WITH_SUPER(BP_Projectile, AActor)
+
+}   // namespace tkd
