7338c2b07b10d18107e06b5408c85bc4bb1a997b
mallory-scotton <mallory.scotton@epitech.eu>
2025-10-07 20:59:23 +0200

    feat: enhance BP_Player with animation handling and improved movement logic

--- Game/Source/Core/Player/BP_Player.cpp
+++ Game/Source/Core/Player/BP_Player.cpp
@@ -12,50 +12,100 @@ namespace tkd
 ///////////////////////////////////////////////////////////////////////////////
 BP_Player::BP_Player(void)
     : APawn()
-    , speed(*this, "Speed", 600.0f)
+    , speed(*this, "Speed", 200.0f)
+    , velocity(*this, "Velocity", FVector2f::Zero)
+    , m_lastVelocity(FVector2f::Zero)
 {
-    auto SpriteComponent = AddComponent<USpriteComponent>("SpriteComponent");
+    auto SpriteComponent =
+        AddComponent<UAnimatedSpriteComponent>("AnimatedSpriteComponent");
     SpriteComponent->SetTexturePath("Game/Assets/Images/T_PlayerShips.png");
+
+    // Define IDLE animation
+    FAnimation2D A_IDLE("IDLE", true);
+    A_IDLE.AddFrame(FAnimation2DFrame(FRectanglei(66, 0, 33, 17), 1.f));
+    SpriteComponent->AddAnimation(A_IDLE);
+
+    // Define IDLE_TO_FLY_UP animation
+    FAnimation2D A_ITFU("IDLE_TO_FLY_UP", false);
+    A_ITFU.AddFrame(FAnimation2DFrame(FRectanglei(33, 0, 33, 17), 0.1f));
+    A_ITFU.AddFrame(FAnimation2DFrame(FRectanglei(0, 0, 33, 17), 1.0f));
+    SpriteComponent->AddAnimation(A_ITFU);
+
+    // Define FLY_UP_TO_IDLE animation
+    FAnimation2D A_FUTI("FLY_UP_TO_IDLE", false);
+    A_FUTI.AddFrame(FAnimation2DFrame(FRectanglei(33, 0, 33, 17), 0.1f));
+    A_FUTI.AddFrame(FAnimation2DFrame(FRectanglei(66, 0, 33, 17), 1.0f));
+    SpriteComponent->AddAnimation(A_FUTI);
+
+    // Define IDLE_TO_FLY_DOWN animation
+    FAnimation2D A_ITFD("IDLE_TO_FLY_DOWN", false);
+    A_ITFD.AddFrame(FAnimation2DFrame(FRectanglei(99, 0, 33, 17), 0.1f));
+    A_ITFD.AddFrame(FAnimation2DFrame(FRectanglei(132, 0, 33, 17), 1.0f));
+    SpriteComponent->AddAnimation(A_ITFD);
+
+    // Define FLY_DOWN_TO_IDLE animation
+    FAnimation2D A_FDTI("FLY_DOWN_TO_IDLE", false);
+    A_FDTI.AddFrame(FAnimation2DFrame(FRectanglei(99, 0, 33, 17), 0.1f));
+    A_FDTI.AddFrame(FAnimation2DFrame(FRectanglei(66, 0, 33, 17), 1.0f));
+    SpriteComponent->AddAnimation(A_FDTI);
+
+    // Set the default animation to IDLE
+    SpriteComponent->Play("IDLE", true);
+
+    // Local transform to scale up the sprite
+    SpriteComponent->SetLocalTransform(
+        FTransform2D(FVector2f::Zero, 0.0f, FVector2f(2.0f, 2.0f))
+    );
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 void BP_Player::Tick(Float32 deltaTime)
 {
-    TKD_UNUSED(deltaTime);
-    // auto hAxis = Engine::Inputs.GetAxis("HorizontalMoves");
-    // auto vAxis = Engine::Inputs.GetAxis("VerticalMoves");
-    // auto fire = Engine::Inputs.GetAction("Fire");
+    // Call Super Tick
+    Super::Tick(deltaTime);
 
-    // if (hAxis)
-    // {
-    //     float scale = hAxis->GetScale();
-    //     if (scale != 0.0f)
-    //     {
-    //         FTransform transform = GetTransform();
-    //         FVector3 position = transform.GetPosition();
-    //         position.x += scale * speed * deltaTime;
-    //         transform.SetPosition(position);
-    //         SetTransform(transform);
-    //     }
-    // }
+    auto vel = velocity.GetValue();
+    auto SpriteComponent =
+        GetComponent<UAnimatedSpriteComponent>("AnimatedSpriteComponent");
 
-    // if (vAxis)
-    // {
-    //     float scale = vAxis->GetScale();
-    //     if (scale != 0.0f)
-    //     {
-    //         FTransform transform = GetTransform();
-    //         FVector3 position = transform.GetPosition();
-    //         position.y += scale * speed * deltaTime;
-    //         transform.SetPosition(position);
-    //         SetTransform(transform);
-    //     }
-    // }
+    // Move player based on velocity and speed
+    if (vel != 0.0f)
+    {
+        // Normalize velocity to ensure consistent speed in all directions
+        if (vel.Length() != 0.0f) { vel = vel.Normalized(); }
 
-    // if (fire)
-    // {
-    //     // Process fire action input
-    // }
+        // Update transform
+        FTransform transform = GetTransform();
+        transform.Translate(FVector3(
+            vel.x * (speed * 1.5f) * deltaTime, vel.y * speed * deltaTime, 0.0f
+        ));
+        SetTransform(transform);
+    }
+
+    // Update animation state
+    if (vel.y > 0.0f) { SpriteComponent->Play("IDLE_TO_FLY_UP", false); }
+    else if (vel.y < 0.0f)
+    {
+        SpriteComponent->Play("IDLE_TO_FLY_DOWN", false);
+    }
+    else
+    {
+        if (m_lastVelocity.y > 0.0f)
+        {
+            SpriteComponent->Play("FLY_UP_TO_IDLE", false);
+        }
+        else if (m_lastVelocity.y < 0.0f)
+        {
+            SpriteComponent->Play("FLY_DOWN_TO_IDLE", false);
+        }
+        else { SpriteComponent->Play("IDLE", true); }
+    }
+
+    // Update last velocity if there is movement
+    if (vel != FVector2f::Zero) { m_lastVelocity = velocity; }
+
+    // Reset velocity for next frame
+    velocity = FVector2f::Zero;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
