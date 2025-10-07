47990061f23d623c56615634141f86365cd740b3
mallory-scotton <mallory.scotton@epitech.eu>
2025-10-07 17:16:41 +0200

    refactor: simplify BP_PlayerController constructor and update input binding setup

--- Game/Source/Core/Player/BP_PlayerController.cpp
+++ Game/Source/Core/Player/BP_PlayerController.cpp
@@ -10,14 +10,47 @@ namespace tkd
 {
 
 ///////////////////////////////////////////////////////////////////////////////
-BP_PlayerController::BP_PlayerController(const FString& name)
-    : APlayerController(name)
+BP_PlayerController::BP_PlayerController()
+    : APlayerController()
 {}
 
 ///////////////////////////////////////////////////////////////////////////////
 void BP_PlayerController::SetupInputBindings(void)
 {
-    // Override this in derived classes to set up your input bindings
+    APawn* pawn = GetPawn();
+
+    if (pawn == nullptr) { return; }
+
+    if (BP_Player* player = dynamic_cast<BP_Player*>(pawn))
+    {
+        BindActionPressed("Fire", [&player](EInput) {});
+
+        BindAxis(
+            "HorizontalMoves",
+            [&player](float scale)
+            {
+                FTransform transform = player->GetTransform();
+                transform.Translate(TVector3<float>(scale, 0.0f, 0.0f));
+                player->SetTransform(transform);
+                std::cout << "Player (" << player->GetObjectID()
+                          << ") position: "
+                          << player->GetTransform().GetPosition() << std::endl;
+            }
+        );
+
+        BindAxis(
+            "VerticalMoves",
+            [&player](float scale)
+            {
+                FTransform transform = player->GetTransform();
+                transform.Translate(TVector3<float>(0.0f, scale, 0.0f));
+                player->SetTransform(transform);
+                std::cout << "Player (" << player->GetObjectID()
+                          << ") position: "
+                          << player->GetTransform().GetPosition() << std::endl;
+            }
+        );
+    }
 }
 
 ///////////////////////////////////////////////////////////////////////////////
