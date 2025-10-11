ad133cb233af9bb64f32c55798df560b9034a5b8
mallory-scotton <mallory.scotton@epitech.eu>
2025-10-11 16:48:55 +0200

    add: include RemoteProcedureCall and Replication headers in Packets.hpp

--- Engine/Source/Engine/Network/Packets.hpp
+++ Engine/Source/Engine/Network/Packets.hpp
@@ -9,3 +9,5 @@
 #include <Engine/Network/Packets/Connect.hpp>
 #include <Engine/Network/Packets/Disconnect.hpp>
 #include <Engine/Network/Packets/HeartBeat.hpp>
+#include <Engine/Network/Packets/RemoteProcedureCall.hpp>
+#include <Engine/Network/Packets/Replication.hpp>
