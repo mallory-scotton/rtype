0d3355a62f272145eedc371b92cac1934ab1bad0
mallory-scotton <mallory.scotton@epitech.eu>
2025-10-18 09:19:42 +0200

    refactor: Update generateOperator function to handle single output type and improve snippet generation

--- Tools/TKDGraph/src/constants/Operators.ts
+++ Tools/TKDGraph/src/constants/Operators.ts
@@ -1,6 +1,7 @@
 /** Dependencies */
 import type { PinType } from '../types';
 import type { NodeTemplate } from '../utils/NodeRegistry';
+import { convertPinTypeToEngineType, convertOperatorSymbolToSnippetOperator } from '../utils';
 
 /**
  * @brief Generates an operator node template.
@@ -9,8 +10,8 @@ import type { NodeTemplate } from '../utils/NodeRegistry';
  * @param operator - The operator symbol.
  * @returns The generated node template.
  */
-function generateOperator(inputs: PinType[], output: PinType[], operator: string): NodeTemplate {
-  if (inputs.length === 0 || output.length === 0) {
+function generateOperator(inputs: PinType[], output: PinType, operator: string): NodeTemplate {
+  if (inputs.length === 0 || !output) {
     throw new Error('Operator must have at least one input and one output.');
   }
 
@@ -40,9 +41,11 @@ function generateOperator(inputs: PinType[], output: PinType[], operator: string
     tags.push('text', 'string', 'concatenation');
   }
 
+  let snippetOperator = convertOperatorSymbolToSnippetOperator(operator);
+
   return {
-    id: `operator_${operator}-${inputs.join('_')}-${output.join('_')}`,
-    name: `${inputs.join(` ${operator} `)} -> ${output.join(' ')}`,
+    id: `operator_${operator}-${inputs.join('_')}-${output}`,
+    name: `${inputs.join(` ${operator} `)} -> ${output}`,
     category: 'Operators',
     data: {
       id: '',
@@ -51,14 +54,21 @@ function generateOperator(inputs: PinType[], output: PinType[], operator: string
         id: '',
         type
       })),
-      outputs: output.map((type) => ({
-        id: '',
-        type
-      })),
+      outputs: [
+        {
+          id: '',
+          type: output
+        }
+      ],
       label: operator,
       type: 'operator'
     },
-    tags: tags
+    tags: tags,
+    snippet: `${convertPinTypeToEngineType(output)} {RESULT} = ${
+      inputs.length > 1
+        ? inputs.map((_, index) => `{${'ABCDEFGHIJKLMNOPQRSTUVWXYZ'[index]}}`).join(` ${snippetOperator} `)
+        : `${snippetOperator}{A}`
+    };`
   };
 }
 
@@ -66,88 +76,87 @@ function generateOperator(inputs: PinType[], output: PinType[], operator: string
  * @brief List of operator node templates.
  */
 export const Operators: NodeTemplate[] = [
-  generateOperator(['float', 'float'], ['float'], '+'),
-  generateOperator(['float', 'float'], ['float'], '-'),
-  generateOperator(['float', 'float'], ['float'], '×'),
-  generateOperator(['float', 'float'], ['float'], '÷'),
-  generateOperator(['float', 'float'], ['bool'], '<'),
-  generateOperator(['float', 'float'], ['bool'], '>'),
-  generateOperator(['float', 'float'], ['bool'], '<='),
-  generateOperator(['float', 'float'], ['bool'], '>='),
-  generateOperator(['float', 'float'], ['bool'], '=='),
-  generateOperator(['float', 'float'], ['bool'], '!='),
-  generateOperator(['int', 'int'], ['int'], '+'),
-  generateOperator(['int', 'int'], ['int'], '-'),
-  generateOperator(['int', 'int'], ['int'], '×'),
-  generateOperator(['int', 'int'], ['int'], '÷'),
-  generateOperator(['int', 'int'], ['bool'], '<'),
-  generateOperator(['int', 'int'], ['bool'], '>'),
-  generateOperator(['int', 'int'], ['bool'], '<='),
-  generateOperator(['int', 'int'], ['bool'], '>='),
-  generateOperator(['int', 'int'], ['bool'], '=='),
-  generateOperator(['int', 'int'], ['bool'], '!='),
-  generateOperator(['real', 'real'], ['real'], '+'),
-  generateOperator(['real', 'real'], ['real'], '-'),
-  generateOperator(['real', 'real'], ['real'], '×'),
-  generateOperator(['real', 'real'], ['real'], '÷'),
-  generateOperator(['real', 'real'], ['bool'], '<'),
-  generateOperator(['real', 'real'], ['bool'], '>'),
-  generateOperator(['real', 'real'], ['bool'], '<='),
-  generateOperator(['real', 'real'], ['bool'], '>='),
-  generateOperator(['real', 'real'], ['bool'], '=='),
-  generateOperator(['real', 'real'], ['bool'], '!='),
-  generateOperator(['byte', 'byte'], ['byte'], '+'),
-  generateOperator(['byte', 'byte'], ['byte'], '-'),
-  generateOperator(['byte', 'byte'], ['byte'], '×'),
-  generateOperator(['byte', 'byte'], ['byte'], '÷'),
-  generateOperator(['byte', 'byte'], ['bool'], '<'),
-  generateOperator(['byte', 'byte'], ['bool'], '>'),
-  generateOperator(['byte', 'byte'], ['bool'], '<='),
-  generateOperator(['byte', 'byte'], ['bool'], '>='),
-  generateOperator(['byte', 'byte'], ['bool'], '=='),
-  generateOperator(['byte', 'byte'], ['bool'], '!='),
-  generateOperator(['vector', 'vector'], ['vector'], '+'),
-  generateOperator(['vector', 'vector'], ['vector'], '-'),
-  generateOperator(['vector', 'vector'], ['vector'], '×'),
-  generateOperator(['vector', 'vector'], ['vector'], '÷'),
-  generateOperator(['vector', 'vector'], ['bool'], '<'),
-  generateOperator(['vector', 'vector'], ['bool'], '>'),
-  generateOperator(['vector', 'vector'], ['bool'], '<='),
-  generateOperator(['vector', 'vector'], ['bool'], '>='),
-  generateOperator(['vector', 'vector'], ['bool'], '=='),
-  generateOperator(['vector', 'vector'], ['bool'], '!='),
-  generateOperator(['rotator', 'rotator'], ['rotator'], '+'),
-  generateOperator(['rotator', 'rotator'], ['rotator'], '+'),
-  generateOperator(['rotator', 'rotator'], ['rotator'], '-'),
-  generateOperator(['rotator', 'rotator'], ['rotator'], '×'),
-  generateOperator(['rotator', 'rotator'], ['rotator'], '÷'),
-  generateOperator(['rotator', 'rotator'], ['bool'], '<'),
-  generateOperator(['rotator', 'rotator'], ['bool'], '>'),
-  generateOperator(['rotator', 'rotator'], ['bool'], '<='),
-  generateOperator(['rotator', 'rotator'], ['bool'], '>='),
-  generateOperator(['rotator', 'rotator'], ['bool'], '=='),
-  generateOperator(['rotator', 'rotator'], ['bool'], '!='),
-  generateOperator(['transform', 'transform'], ['transform'], '+'),
-  generateOperator(['transform', 'transform'], ['transform'], '-'),
-  generateOperator(['transform', 'transform'], ['transform'], '×'),
-  generateOperator(['transform', 'transform'], ['transform'], '÷'),
-  generateOperator(['transform', 'transform'], ['bool'], '<'),
-  generateOperator(['transform', 'transform'], ['bool'], '>'),
-  generateOperator(['transform', 'transform'], ['bool'], '<='),
-  generateOperator(['transform', 'transform'], ['bool'], '>='),
-  generateOperator(['transform', 'transform'], ['bool'], '=='),
-  generateOperator(['transform', 'transform'], ['bool'], '!='),
-  generateOperator(['bool', 'bool'], ['bool'], 'AND'),
-  generateOperator(['bool', 'bool'], ['bool'], 'OR'),
-  generateOperator(['bool', 'bool'], ['bool'], 'XOR'),
-  generateOperator(['bool'], ['bool'], 'NOT'),
-  generateOperator(['string', 'string'], ['string'], '+'),
-  generateOperator(['string', 'string'], ['bool'], '=='),
-  generateOperator(['string', 'string'], ['bool'], '!='),
-  generateOperator(['text', 'text'], ['text'], '+'),
-  generateOperator(['text', 'text'], ['bool'], '=='),
-  generateOperator(['text', 'text'], ['bool'], '!='),
-  generateOperator(['name', 'name'], ['name'], '+'),
-  generateOperator(['name', 'name'], ['bool'], '=='),
-  generateOperator(['name', 'name'], ['bool'], '!=')
+  generateOperator(['float', 'float'], 'float', '+'),
+  generateOperator(['float', 'float'], 'float', '-'),
+  generateOperator(['float', 'float'], 'float', '×'),
+  generateOperator(['float', 'float'], 'float', '÷'),
+  generateOperator(['float', 'float'], 'bool', '<'),
+  generateOperator(['float', 'float'], 'bool', '>'),
+  generateOperator(['float', 'float'], 'bool', '<='),
+  generateOperator(['float', 'float'], 'bool', '>='),
+  generateOperator(['float', 'float'], 'bool', '=='),
+  generateOperator(['float', 'float'], 'bool', '!='),
+  generateOperator(['int', 'int'], 'int', '+'),
+  generateOperator(['int', 'int'], 'int', '-'),
+  generateOperator(['int', 'int'], 'int', '×'),
+  generateOperator(['int', 'int'], 'int', '÷'),
+  generateOperator(['int', 'int'], 'bool', '<'),
+  generateOperator(['int', 'int'], 'bool', '>'),
+  generateOperator(['int', 'int'], 'bool', '<='),
+  generateOperator(['int', 'int'], 'bool', '>='),
+  generateOperator(['int', 'int'], 'bool', '=='),
+  generateOperator(['int', 'int'], 'bool', '!='),
+  generateOperator(['real', 'real'], 'real', '+'),
+  generateOperator(['real', 'real'], 'real', '-'),
+  generateOperator(['real', 'real'], 'real', '×'),
+  generateOperator(['real', 'real'], 'real', '÷'),
+  generateOperator(['real', 'real'], 'bool', '<'),
+  generateOperator(['real', 'real'], 'bool', '>'),
+  generateOperator(['real', 'real'], 'bool', '<='),
+  generateOperator(['real', 'real'], 'bool', '>='),
+  generateOperator(['real', 'real'], 'bool', '=='),
+  generateOperator(['real', 'real'], 'bool', '!='),
+  generateOperator(['byte', 'byte'], 'byte', '+'),
+  generateOperator(['byte', 'byte'], 'byte', '-'),
+  generateOperator(['byte', 'byte'], 'byte', '×'),
+  generateOperator(['byte', 'byte'], 'byte', '÷'),
+  generateOperator(['byte', 'byte'], 'bool', '<'),
+  generateOperator(['byte', 'byte'], 'bool', '>'),
+  generateOperator(['byte', 'byte'], 'bool', '<='),
+  generateOperator(['byte', 'byte'], 'bool', '>='),
+  generateOperator(['byte', 'byte'], 'bool', '=='),
+  generateOperator(['byte', 'byte'], 'bool', '!='),
+  generateOperator(['vector', 'vector'], 'vector', '+'),
+  generateOperator(['vector', 'vector'], 'vector', '-'),
+  generateOperator(['vector', 'vector'], 'vector', '×'),
+  generateOperator(['vector', 'vector'], 'vector', '÷'),
+  generateOperator(['vector', 'vector'], 'bool', '<'),
+  generateOperator(['vector', 'vector'], 'bool', '>'),
+  generateOperator(['vector', 'vector'], 'bool', '<='),
+  generateOperator(['vector', 'vector'], 'bool', '>='),
+  generateOperator(['vector', 'vector'], 'bool', '=='),
+  generateOperator(['vector', 'vector'], 'bool', '!='),
+  generateOperator(['rotator', 'rotator'], 'rotator', '+'),
+  generateOperator(['rotator', 'rotator'], 'rotator', '-'),
+  generateOperator(['rotator', 'rotator'], 'rotator', '×'),
+  generateOperator(['rotator', 'rotator'], 'rotator', '÷'),
+  generateOperator(['rotator', 'rotator'], 'bool', '<'),
+  generateOperator(['rotator', 'rotator'], 'bool', '>'),
+  generateOperator(['rotator', 'rotator'], 'bool', '<='),
+  generateOperator(['rotator', 'rotator'], 'bool', '>='),
+  generateOperator(['rotator', 'rotator'], 'bool', '=='),
+  generateOperator(['rotator', 'rotator'], 'bool', '!='),
+  generateOperator(['transform', 'transform'], 'transform', '+'),
+  generateOperator(['transform', 'transform'], 'transform', '-'),
+  generateOperator(['transform', 'transform'], 'transform', '×'),
+  generateOperator(['transform', 'transform'], 'transform', '÷'),
+  generateOperator(['transform', 'transform'], 'bool', '<'),
+  generateOperator(['transform', 'transform'], 'bool', '>'),
+  generateOperator(['transform', 'transform'], 'bool', '<='),
+  generateOperator(['transform', 'transform'], 'bool', '>='),
+  generateOperator(['transform', 'transform'], 'bool', '=='),
+  generateOperator(['transform', 'transform'], 'bool', '!='),
+  generateOperator(['bool', 'bool'], 'bool', 'AND'),
+  generateOperator(['bool', 'bool'], 'bool', 'OR'),
+  generateOperator(['bool', 'bool'], 'bool', 'XOR'),
+  generateOperator(['bool'], 'bool', 'NOT'),
+  generateOperator(['string', 'string'], 'string', '+'),
+  generateOperator(['string', 'string'], 'bool', '=='),
+  generateOperator(['string', 'string'], 'bool', '!='),
+  generateOperator(['text', 'text'], 'text', '+'),
+  generateOperator(['text', 'text'], 'bool', '=='),
+  generateOperator(['text', 'text'], 'bool', '!='),
+  generateOperator(['name', 'name'], 'name', '+'),
+  generateOperator(['name', 'name'], 'bool', '=='),
+  generateOperator(['name', 'name'], 'bool', '!=')
 ];
