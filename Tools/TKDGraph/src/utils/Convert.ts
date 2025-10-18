9119ef4bb36bb2e85fddbb58640c7103999c2eeb
mallory-scotton <mallory.scotton@epitech.eu>
2025-10-18 09:19:46 +0200

    feat: Add conversion functions for pin types and operator symbols

---
+++ Tools/TKDGraph/src/utils/Convert.ts
@@ -0,0 +1,51 @@
+/** Dependencies */
+import type { PinType } from '../types';
+
+/**
+ * @brief Converts a pin type to its corresponding engine type.
+ * @param pinType - The pin type to convert.
+ * @returns The corresponding engine type as a string.
+ */
+export function convertPinTypeToEngineType(pinType: PinType): string {
+  switch (pinType) {
+    case 'byte':
+      return 'Byte';
+    case 'int':
+      return 'Int32';
+    case 'float':
+      return 'Float32';
+    case 'real':
+      return 'Int64';
+    case 'bool':
+      return 'Bool';
+    case 'string':
+    case 'text':
+    case 'name':
+      return 'FString';
+    case 'vector':
+      return 'FVector';
+    case 'rotator':
+      return 'FRotator';
+    case 'transform':
+      return 'FTransform';
+    case 'object':
+      return 'UObject*';
+    default:
+      return 'auto';
+  }
+}
+
+/**
+ * @brief Converts an operator symbol to its corresponding snippet operator.
+ * @param operator - The operator symbol to convert.
+ * @returns The corresponding snippet operator as a string.
+ */
+export function convertOperatorSymbolToSnippetOperator(operator: string): string {
+  if (operator === '×') return '*';
+  else if (operator === '÷') return '/';
+  else if (operator === 'AND') return '&&';
+  else if (operator === 'OR') return '||';
+  else if (operator === 'XOR') return '^';
+  else if (operator === 'NOT') return '!';
+  return operator;
+}
