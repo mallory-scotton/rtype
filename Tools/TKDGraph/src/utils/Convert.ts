88874a35a2ba7bd7c3f4ac7e82b3d329b60a7387
mallory-scotton <mallory.scotton@epitech.eu>
2025-10-18 17:05:39 +0200

    fix: Ensure default value for vector, rotator, and transform types is an empty string

--- Tools/TKDGraph/src/utils/Convert.ts
+++ Tools/TKDGraph/src/utils/Convert.ts
@@ -56,6 +56,7 @@ export function convertPinTypeToDefaultValue(pinType: PinType): any {
     case 'vector':
     case 'rotator':
     case 'transform':
+      return '';
     case 'object':
       return null;
     default:
