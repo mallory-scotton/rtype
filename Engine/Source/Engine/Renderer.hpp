b42d823aed0d9ed90c01d5d0e0257b84c7836c87
mallory-scotton <mallory.scotton@epitech.eu>
2025-10-22 17:48:39 +0200

    feat: Update Renderer.hpp to include additional dependencies for rendering

--- Engine/Source/Engine/Renderer.hpp
+++ Engine/Source/Engine/Renderer.hpp
@@ -7,12 +7,16 @@
 // Dependencies
 ///////////////////////////////////////////////////////////////////////////////
 #include <Engine/Renderer/Enumerations.hpp>
+#include <Engine/Renderer/FCamera.hpp>
 #include <Engine/Renderer/FRenderStates.hpp>
+#include <Engine/Renderer/FTransformable.hpp>
 #include <Engine/Renderer/FTransformable2D.hpp>
 #include <Engine/Renderer/FVertex2DArray.hpp>
 #include <Engine/Renderer/FView.hpp>
 #include <Engine/Renderer/Interfaces.hpp>
+#include <Engine/Renderer/Primitives.hpp>
 #include <Engine/Renderer/SFML.hpp>
 #include <Engine/Renderer/Shapes.hpp>
+#include <Engine/Renderer/UPrimitive.hpp>
 #include <Engine/Renderer/UShape.hpp>
 #include <Engine/Renderer/USprite.hpp>
