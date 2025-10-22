c8de3c35908f8915c184c354c51098b144cafbb7
mallory-scotton <mallory.scotton@epitech.eu>
2025-10-22 09:44:12 +0200

    feat: Add ACamera class with properties for view transform, field of view, aspect ratio, and clipping planes

--- Engine/Source/Engine/Runtime/Actor.hpp
+++ Engine/Source/Engine/Runtime/Actor.hpp
@@ -7,5 +7,6 @@
 // Dependencies
 ///////////////////////////////////////////////////////////////////////////////
 #include <Engine/Runtime/Actor/AActor.hpp>
+#include <Engine/Runtime/Actor/ACamera.hpp>
 #include <Engine/Runtime/Actor/AHUD.hpp>
 #include <Engine/Runtime/Actor/APawn.hpp>
