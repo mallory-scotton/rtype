ef23a23859ed5e3a79a1c37ad287ae46cb5e1e0c
mallory-scotton <mallory.scotton@epitech.eu>
2025-10-24 01:41:03 +0200

    feat: Implement transformation matrix calculation in FPose class

--- Engine/Source/Engine/Renderer/VR/FPose.cpp
+++ Engine/Source/Engine/Renderer/VR/FPose.cpp
@@ -36,16 +36,50 @@ FPose::FPose(
 ///////////////////////////////////////////////////////////////////////////////
 FMatrix4x4 FPose::GetMatrix(void) const
 {
-    // TODO: Implement transformation matrix calculation from position and
-    // rotation
-    return FMatrix4x4();
+    // Convert quaternion to rotation matrix
+    float x = rotation.GetX();
+    float y = rotation.GetY();
+    float z = rotation.GetZ();
+    float w = rotation.GetW();
+
+    float x2 = x * x;
+    float y2 = y * y;
+    float z2 = z * z;
+    float xy = x * y;
+    float xz = x * z;
+    float yz = y * z;
+    float wx = w * x;
+    float wy = w * y;
+    float wz = w * z;
+
+    // Create rotation matrix from quaternion
+    FMatrix4x4 mat(
+        1.0f - 2.0f * (y2 + z2),
+        2.0f * (xy - wz),
+        2.0f * (xz + wy),
+        0.0f,
+        2.0f * (xy + wz),
+        1.0f - 2.0f * (x2 + z2),
+        2.0f * (yz - wx),
+        0.0f,
+        2.0f * (xz - wy),
+        2.0f * (yz + wx),
+        1.0f - 2.0f * (x2 + y2),
+        0.0f,
+        position.x,
+        position.y,
+        position.z,
+        1.0f
+    );
+
+    return mat;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 FMatrix4x4 FPose::GetViewMatrix(void) const
 {
-    // TODO: Implement view matrix calculation from position and rotation
-    return FMatrix4x4();
+    // View matrix is the inverse of the transformation matrix
+    return GetMatrix().Inverse();
 }
 
 }   // namespace tkd::VR
