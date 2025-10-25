a54279bf6e9f3571f4257a2df3576509b3d64700
mallory-scotton <mallory.scotton@epitech.eu>
2025-10-25 11:37:43 +0200

    feat: Add capsule, cone, cylinder, prism, pyramid, and torus primitive headers

--- Engine/Source/Engine/Renderer/Primitives.hpp
+++ Engine/Source/Engine/Renderer/Primitives.hpp
@@ -7,6 +7,12 @@
 // Dependencies
 ///////////////////////////////////////////////////////////////////////////////
 #include <Engine/Renderer/Primitives/UBillboardPrimitive.hpp>
+#include <Engine/Renderer/Primitives/UCapsulePrimitive.hpp>
+#include <Engine/Renderer/Primitives/UConePrimitive.hpp>
 #include <Engine/Renderer/Primitives/UCubePrimitive.hpp>
+#include <Engine/Renderer/Primitives/UCylinderPrimitive.hpp>
 #include <Engine/Renderer/Primitives/UPlanePrimitive.hpp>
+#include <Engine/Renderer/Primitives/UPrismPrimitive.hpp>
+#include <Engine/Renderer/Primitives/UPyramidPrimitive.hpp>
 #include <Engine/Renderer/Primitives/USpherePrimitive.hpp>
+#include <Engine/Renderer/Primitives/UTorusPrimitive.hpp>
