87efc55f01f5045084d42bdecd886ca51be62222
mallory-scotton <mallory.scotton@epitech.eu>
2025-10-25 14:03:40 +0200

    feat: Add missing component includes for 3D primitive representations

--- Engine/Source/Engine/Runtime/Components.hpp
+++ Engine/Source/Engine/Runtime/Components.hpp
@@ -9,4 +9,14 @@
 #include <Engine/Runtime/Components/Collisions.hpp>
 #include <Engine/Runtime/Components/UActorComponent.hpp>
 #include <Engine/Runtime/Components/UAnimatedSpriteComponent.hpp>
+#include <Engine/Runtime/Components/UBillboardComponent.hpp>
+#include <Engine/Runtime/Components/UCapsuleComponent.hpp>
+#include <Engine/Runtime/Components/UConeComponent.hpp>
+#include <Engine/Runtime/Components/UCubeComponent.hpp>
+#include <Engine/Runtime/Components/UCylinderComponent.hpp>
+#include <Engine/Runtime/Components/UPlaneComponent.hpp>
+#include <Engine/Runtime/Components/UPrismComponent.hpp>
+#include <Engine/Runtime/Components/UPyramidComponent.hpp>
+#include <Engine/Runtime/Components/USphereComponent.hpp>
 #include <Engine/Runtime/Components/USpriteComponent.hpp>
+#include <Engine/Runtime/Components/UTorusComponent.hpp>
