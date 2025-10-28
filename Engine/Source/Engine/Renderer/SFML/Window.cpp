97bab7e9a862d812624ff6b627fcbf6ce50b5be4
nathan <nathan.fievet@epitech.eu>
2025-10-29 00:14:14 +0100

    fix: add debug log for window closing event

--- Engine/Source/Engine/Renderer/SFML/Window.cpp
+++ Engine/Source/Engine/Renderer/SFML/Window.cpp
@@ -178,6 +178,7 @@ void Window::SetDebugMode(bool debugMode)
 ///////////////////////////////////////////////////////////////////////////////
 bool Window::Close(void)
 {
+    std::cout << "[WINDOW] we closing the window" << std::endl;
     // Check if the window is open
     if (!IsOpen()) { return false; }
 
