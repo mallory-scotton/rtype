b24b9ab3c6ff1efca8250a3c38b780077d7e0ddc
mallory-scotton <mallory.scotton@epitech.eu>
2025-10-30 20:02:07 +0100

    fix: Refactor PlaySound and PlaySound3D methods to return TSharedPtr<IAudioSource> and use shared_mutex for thread safety

--- Engine/Source/Engine/Static/FAudioInterface.cpp
+++ Engine/Source/Engine/Static/FAudioInterface.cpp
@@ -2,6 +2,7 @@
 // Dependencies
 ///////////////////////////////////////////////////////////////////////////////
 #include <Engine/Static/FAudioInterface.hpp>
+#include <shared_mutex>
 
 ///////////////////////////////////////////////////////////////////////////////
 // Namespace tkd
@@ -11,7 +12,7 @@ namespace tkd
 
 ///////////////////////////////////////////////////////////////////////////////
 TUniquePtr<IAudioManager> FAudioInterface::s_manager = nullptr;
-std::mutex FAudioInterface::s_mutex;
+std::shared_mutex FAudioInterface::s_mutex;
 
 ///////////////////////////////////////////////////////////////////////////////
 Bool FAudioInterface::Initialize(void)
@@ -32,13 +33,92 @@ IAudioManager* FAudioInterface::GetAudioManager(void)
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-void FAudioInterface::PlaySound(
+TSharedPtr<IAudioSource> FAudioInterface::PlaySound(
     const FilePath& filePath, Float32 volume, Bool loop
 )
 {
-    std::lock_guard lock(s_mutex);
-    if (!s_manager) { return; }
-    s_manager->PlaySound(filePath, volume, loop);
+    std::shared_lock lock(s_mutex);
+    if (!s_manager) { return nullptr; }
+    return s_manager->PlaySound(filePath, volume, loop);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+TSharedPtr<IAudioSource> FAudioInterface::PlaySound(
+    const std::vector<Byte>& data, Float32 volume, Bool loop
+)
+{
+    std::shared_lock lock(s_mutex);
+    if (!s_manager) { return nullptr; }
+    return s_manager->PlaySound(data, volume, loop);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+TSharedPtr<IAudioSource> FAudioInterface::PlaySound(
+    const Byte* data, SizeT size, Float32 volume, Bool loop
+)
+{
+    std::shared_lock lock(s_mutex);
+    if (!s_manager) { return nullptr; }
+    return s_manager->PlaySound(data, size, volume, loop);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+TSharedPtr<IAudioSource>
+    FAudioInterface::PlaySound(const UAsset* asset, Float32 volume, Bool loop)
+{
+    std::shared_lock lock(s_mutex);
+    if (!s_manager) { return nullptr; }
+    return s_manager->PlaySound(asset, volume, loop);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+TSharedPtr<IAudioSource> FAudioInterface::PlaySound3D(
+    const FilePath& filePath,
+    const FVector3& position,
+    Float32 volume,
+    Bool loop
+)
+{
+    std::shared_lock lock(s_mutex);
+    if (!s_manager) { return nullptr; }
+    return s_manager->PlaySound3D(filePath, position, volume, loop);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+TSharedPtr<IAudioSource> FAudioInterface::PlaySound3D(
+    const std::vector<Byte>& data,
+    const FVector3& position,
+    Float32 volume,
+    Bool loop
+)
+{
+    std::shared_lock lock(s_mutex);
+    if (!s_manager) { return nullptr; }
+    return s_manager->PlaySound3D(data, position, volume, loop);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+TSharedPtr<IAudioSource> FAudioInterface::PlaySound3D(
+    const Byte* data,
+    SizeT size,
+    const FVector3& position,
+    Float32 volume,
+    Bool loop
+)
+{
+    std::shared_lock lock(s_mutex);
+    if (!s_manager) { return nullptr; }
+    return s_manager->PlaySound3D(data, size, position, volume, loop);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+TSharedPtr<IAudioSource> FAudioInterface::PlaySound3D(
+    const UAsset* asset, const FVector3& position, Float32 volume, Bool loop
+)
+{
+    std::shared_lock lock(s_mutex);
+    if (!s_manager) { return nullptr; }
+    return s_manager->PlaySound3D(asset, position, volume, loop);
 }
 
 }   // namespace tkd
