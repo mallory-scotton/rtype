66a01d52792b7a6a5d1a0aac65181573bd0dbf49
Hugo <hugo.cathelain@epitech.eu>
2025-11-10 11:36:47 +0100

    add: more spawner of monsters in the first level

--- Games/RType/Source/Levels/L_RType_Level1.cpp
+++ Games/RType/Source/Levels/L_RType_Level1.cpp
@@ -21,6 +21,21 @@ L_RType_Level1::L_RType_Level1()
     AddActorEntry(
         "BP_MonsterSpawner", "MonsterSpawner1", FTransform::Identity
     );
+    FTransform spawnerTransform1;
+    spawnerTransform1.SetPosition(FVector3(5.0f, 5.0f, 0.0f));
+    AddActorEntry("BP_MonsterSpawner", "MonsterSpawner2", spawnerTransform1);
+    FTransform spawnerTransform2;
+    spawnerTransform2.SetPosition(FVector3(0.0f, 5.0f, 0.0f));
+    AddActorEntry("BP_MonsterSpawner", "MonsterSpawner3", spawnerTransform2);
+    FTransform spawnerTransform3;
+    spawnerTransform3.SetPosition(FVector3(5.0f, 0.0f, 0.0f));
+    AddActorEntry("BP_MonsterSpawner", "MonsterSpawner4", spawnerTransform3);
+    FTransform spawnerTransform4;
+    spawnerTransform4.SetPosition(FVector3(-5.0f, 0.0f, 0.0f));
+    AddActorEntry("BP_MonsterSpawner", "MonsterSpawner5", spawnerTransform4);
+    FTransform spawnerTransform5;
+    spawnerTransform5.SetPosition(FVector3(-5.0f, 5.0f, 0.0f));
+    AddActorEntry("BP_MonsterSpawner", "MonsterSpawner5", spawnerTransform5);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
